19c170c74791b75520bc4dbf132dabd8afd2e879
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-04 16:28:04 +0900

    add: createOrderTest 추가

--- src/test/java/sample/cafekiosk/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/CafeKioskTest.java
@@ -1,8 +1,12 @@
 package sample.cafekiosk;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.CafeKiosk;
 import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.order.Order;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CafeKioskTest {
 
@@ -13,5 +17,18 @@ public class CafeKioskTest {
 
         System.out.println(">>> 담긴 음료 수 : " + cafeKiosk.getBeverages().size());
         System.out.println(">>> 담긴 음료 : " + cafeKiosk.getBeverages().get(0).getName());
+    }
+
+    @Test
+    void createOrder(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        Order order = cafeKiosk.createOrder();
+
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
     }
 }
