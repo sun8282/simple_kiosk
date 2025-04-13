c52fcbbb971871fd7b85b1d2be021be8e1821795
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-13 18:00:07 +0900

    update : OrderProduct

--- src/main/java/sample/cafekiosk/spring/domain/orderproduct/OrderProduct.java
+++ src/main/java/sample/cafekiosk/spring/domain/orderproduct/OrderProduct.java
@@ -1,8 +1,15 @@
 package sample.cafekiosk.spring.domain.orderproduct;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderProduct {
 
 }
