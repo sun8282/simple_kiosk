c5cf03c92fcbff840fce9abc47bf51b552503fc0
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-13 17:27:12 +0900

    add : OrderProduct

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -1,4 +1,26 @@
 package sample.cafekiosk.spring.domain.order;
 
-public class Order {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor
+@Entity
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+
 }
---
+++ src/main/java/sample/cafekiosk/spring/domain/orderproduct/OrderProduct.java
@@ -0,0 +1,8 @@
+package sample.cafekiosk.spring.domain.orderproduct;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class OrderProduct {
+
+}
