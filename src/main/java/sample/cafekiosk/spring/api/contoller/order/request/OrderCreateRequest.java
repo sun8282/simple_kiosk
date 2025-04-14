a4f5b124e8a06fbf0e2add3f92646dc0c1c554f8
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-14 17:32:25 +0900

    fix : OrderCreateRequest default Constructor

--- src/main/java/sample/cafekiosk/spring/api/contoller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/contoller/order/request/OrderCreateRequest.java
@@ -2,10 +2,12 @@ package sample.cafekiosk.spring.api.contoller.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
 
     private List<String> productNumbers;
