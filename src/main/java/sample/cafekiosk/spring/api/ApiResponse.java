16a6a4e1a421cae83dbfb1847345e816e82bb0df
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-17 15:59:51 +0900

    add: ApiResponse

---
+++ src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.spring.api;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return new ApiResponse<>(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(HttpStatus.OK, HttpStatus.OK.name(), data);
+    }
+
+}
