2bf9beda46ac8bf40b77ff9d45a032546d070346
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-21 18:12:17 +0900

    add: sendMailTest

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -1,7 +1,32 @@
 package sample.cafekiosk.spring.api.service.mail;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class MailServiceTest {
+    @Test
+    @DisplayName("메일 전송 테스트")
+    void sendMail() {
+        // given
+        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
+        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
 
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
+                        .thenReturn(true);
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+    }
 }
