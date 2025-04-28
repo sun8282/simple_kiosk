e2bbd58b03febfbdf3bf9a9f8d4b9092ce8cc9d3
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-28 18:49:31 +0900

    add: @MethodSource

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -3,7 +3,10 @@ package sample.cafekiosk.spring.domain.product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,8 +57,16 @@ class ProductTypeTest {
         assertThat(result3).isTrue();
     }
 
+    private static Stream<Arguments> provideProductTypesForCheckingStockType(){
+        return Stream.of(
+                Arguments.of(ProductType.HANDMADE, false),
+                Arguments.of(ProductType.BOTTLE, true),
+                Arguments.of(ProductType.BAKERY, true)
+        );
+    }
+
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
-    @CsvSource({"HANDMADE, flase", "BOTTLE, true", "BAKERY, true"})
+    @MethodSource("provideProductTypesForCheckingStockType")
     @ParameterizedTest
     void containsStockType4(ProductType productType, boolean expected){
 
